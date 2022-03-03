b19fe1c3822ef37e217342009240a64adec24246
Deniz-Navarro <ldeniz1@ucol.mx>
2022-03-03 16:15:49 -0600

    Add menu activity

--- app/src/main/java/com/timrocket/sazonapp/MainActivity.java
+++ app/src/main/java/com/timrocket/sazonapp/MainActivity.java
@@ -2,10 +2,13 @@ package com.timrocket.sazonapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button iniciar;
+    Intent i;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
