04fb9363ec49a38197575c561881988f65080191
Deniz-Navarro <ldeniz1@ucol.mx>
2022-03-07 11:15:29 -0600

    data base conected

--- app/src/main/java/com/timrocket/sazonapp/MainActivity.java
+++ app/src/main/java/com/timrocket/sazonapp/MainActivity.java
@@ -2,13 +2,25 @@ package com.timrocket.sazonapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button start;
+    Intent a;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        start = (Button) findViewById(R.id.btnstart);
+        start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent a = new Intent(MainActivity.this,HomeActivity.class);
+                startActivity(a);
+            }
+        });
     }
 }
