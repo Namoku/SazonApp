4e7161e9441c703dd442e35b4d1290e7d5240030
Namoku <contact@namoku.xyz>
2022-03-07 09:51:15 -0600

    Revert

--- app/src/main/java/com/timrocket/sazonapp/MainActivity.java
+++ app/src/main/java/com/timrocket/sazonapp/MainActivity.java
@@ -2,13 +2,10 @@ package com.timrocket.sazonapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-    Button iniciar;
-    Intent i;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
