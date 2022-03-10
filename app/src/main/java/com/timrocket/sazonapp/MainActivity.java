69b90c1ab0e48542225bb8dad130407c26d73d6e
samuel wuitron mendoza <cwuitron@ucol.mx>
2022-03-09 19:24:19 -0600

    Pantalla de arranque de la aplicación terminada

--- app/src/main/java/com/timrocket/sazonapp/MainActivity.java
+++ app/src/main/java/com/timrocket/sazonapp/MainActivity.java
@@ -4,13 +4,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
+import android.widget.ImageView;
 
+import com.airbnb.lottie.LottieAnimationView;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity  {
 
-    FloatingActionButton fab;
+    ImageView appName, splashImg;
+    LottieAnimationView lottieAnimationView;
+    private static int SPLASH_SCREEN = 5400;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,15 +23,22 @@ public class MainActivity extends AppCompatActivity  {
         setContentView(R.layout.activity_main);
         getSupportActionBar().hide();
 
-        fab = (FloatingActionButton) findViewById(R.id.fab);
+        appName = findViewById(R.id.app_name);
+        splashImg = findViewById(R.id.bg_pink);
+        lottieAnimationView = findViewById(R.id.lottie);
 
-        fab.setOnClickListener(new View.OnClickListener() {
+        splashImg.animate().translationY(-2000).setDuration(1300).setStartDelay(4000);
+        appName.animate().translationY(1800).setDuration(1200).setStartDelay(4000);
+        lottieAnimationView.animate().translationY(1400).setDuration(1000).setStartDelay(4000);
+
+        new Handler().postDelayed(new Runnable() {
             @Override
-            public  void onClick(View v){
-                Intent intent = new Intent(MainActivity.this , LoginActivity.class);
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
                 startActivity(intent);
+                finish();
             }
+        },SPLASH_SCREEN);
 
-        });
     }
 }
