8c73afa18e67024f63ddab8b1b78d33797823ddd
Joana Miguel Gómez <jmiguel@ucol.mx>
2022-03-13 12:59:55 -0600

    menu with fragments

--- app/src/main/java/com/timrocket/sazonapp/HomeActivity.java
+++ app/src/main/java/com/timrocket/sazonapp/HomeActivity.java
@@ -41,7 +41,7 @@ public class HomeActivity extends AppCompatActivity {
     // Passing each menu ID as a set of Ids because each
     // menu should be considered as top level destinations.
     mAppBarConfiguration = new AppBarConfiguration.Builder(
-            R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow)
+            R.id.nav_home, R.id.nav_buscar, R.id.nav_oriental, R.id.nav_antojitos, R.id.nav_vegana, R.id.nav_rapida, R.id.nav_gourmet, R.id.nav_tematica)
             .setOpenableLayout(drawer)
             .build();
     NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_home);
