408027063b78d2c545e9dc8284cd4a0abf721888
Deniz-Navarro <ldeniz1@ucol.mx>
2022-03-22 00:16:47 -0600

    get and view Data

--- app/src/main/java/com/timrocket/sazonapp/FragmentOriental.java
+++ app/src/main/java/com/timrocket/sazonapp/FragmentOriental.java
@@ -5,27 +5,46 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Adapter;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.concurrent.Executor;
 
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link FragmentOriental#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class FragmentOriental extends Fragment {
+public class FragmentOriental extends Fragment implements AdapterView.OnItemClickListener {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
+    private ListView mListView;
+    private List<String> mLista = new ArrayList<>();
+    private ArrayAdapter<String> mAdapter;
+
 
     // TODO: Rename and change types of parameters
     private String mParam1;
@@ -34,18 +53,6 @@ public class FragmentOriental extends Fragment {
     public FragmentOriental() {
         // Required empty public constructor
     }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment FragmentOriental.
-     */
-    // TODO: Rename and change types and number of parameters
-
-
     private void getData (FirebaseFirestore db, String category) {
         db.collection("Restaurants")
                 .whereEqualTo("category", category)
@@ -56,6 +63,9 @@ public class FragmentOriental extends Fragment {
                         if (task.isSuccessful()) {
                             for (QueryDocumentSnapshot document : task.getResult()) {
                                 Log.d("TAG", document.getId() + " => " + document.getData());
+                                mLista.add((String) document.get("name"));
+                                mAdapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1,mLista);
+                                mListView.setAdapter(mAdapter);
                             }
                         } else {
                             Log.w("TAG", "Error getting documents.", task.getException());
@@ -63,6 +73,9 @@ public class FragmentOriental extends Fragment {
                     }
                 });
     }
+
+
+
     public static FragmentOriental newInstance(String param1, String param2) {
         FragmentOriental fragment = new FragmentOriental();
         Bundle args = new Bundle();
@@ -84,9 +97,17 @@ public class FragmentOriental extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        FirebaseFirestore db = FirebaseFirestore.getInstance();
-        getData(db, "oriental");
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_oriental, container, false);
+        View root = inflater.inflate(R.layout.fragment_oriental, container, false);
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        mListView = root.findViewById(R.id.oListView);
+        mListView.setOnItemClickListener(this);
+        getData(db, "oriental");
+        return root;
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+        Toast.makeText(getActivity(), "Item seleccionado: "+ i, Toast.LENGTH_SHORT).show();
     }
 }
